eb5634809ac31969a36f24cc09298c577d89d930
Ryan Plante <rmplante@email.neit.edu>
2023-02-03 13:43:35 -0500

    change the project from mvc to webforms

---
+++ Login.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="labMonitor.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<form id="form1" runat="server">
+  <div>
+    <label for="txtUsername">Username:</label>
+    <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+  </div>
+  <div>
+    <label for="txtPassword">Password:</label>
+    <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+  </div>
+  <div>
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+  </div>
+</form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace labMonitor
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "admin" && password == "password")
+            {
+                Session["IsLoggedIn"] = true;
+                Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                Response.Write("Invalid username or password.");
+            }
+        }
+    }
+}
