20210e07e4b347e23db7cc7713e8b1ce33cf188e
Ryan Plante <rmplante@email.neit.edu>
2023-03-08 14:18:41 -0500

    remove imagebutton for edit image.

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -39,7 +39,7 @@ namespace labMonitor
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton avaImage;
+        protected global::System.Web.UI.WebControls.Image avaImage;
 
         /// <summary>
         /// ImageButton3 control.
